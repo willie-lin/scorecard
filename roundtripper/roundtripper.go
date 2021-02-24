381f1be8b06d8d5a655e3e7a453d4975253874ec
naveen <172697+naveensrinivasan@users.noreply.github.com>
2021-02-24 18:55:46 -0500

    Feature - New ratelimiter implementation
    New implementation of the ratelimiter to handle 403 AbuseRateLimitError and RateLimitError.
    Mostly based on this
    https://github.com/hashicorp/terraform-provider-github/blob/fa73654b66e37b1fd8d886141d9c2974e24ba42f/github/transport.go#L42-L109

--- roundtripper/roundtripper.go
+++ roundtripper/roundtripper.go
@@ -15,7 +15,10 @@
 package roundtripper
 
 import (
+	"bytes"
 	"context"
+	"io"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
@@ -25,6 +28,7 @@ import (
 	"time"
 
 	"github.com/bradleyfalzon/ghinstallation"
+	"github.com/google/go-github/v32/github"
 	cache "github.com/naveensrinivasan/httpcache"
 	"github.com/naveensrinivasan/httpcache/diskcache"
 	"github.com/peterbourgon/diskv"
@@ -42,13 +46,8 @@ const (
 	DiskCachePath           = "DISK_CACHE_PATH"
 	UseBlobCache            = "USE_BLOB_CACHE"
 	BucketURL               = "BLOB_URL"
+	writeDelay              = 1 * time.Second
 )
-
-// RateLimitRoundTripper is a rate-limit aware http.Transport for Github.
-type RateLimitRoundTripper struct {
-	Logger         *zap.SugaredLogger
-	InnerTransport http.RoundTripper
-}
 
 type RoundRobinTokenSource struct {
 	counter      int64
@@ -87,11 +86,7 @@ func NewTransport(ctx context.Context, logger *zap.SugaredLogger) http.RoundTrip
 		}
 	}
 
-	// Wrap that with the rate limiter
-	rateLimit := &RateLimitRoundTripper{
-		Logger:         logger,
-		InnerTransport: transport,
-	}
+	rateLimit := &RateLimitTransport{transport: transport, Logger: logger}
 
 	// uses blob cache like GCS,S3.
 	if cachePath, useBlob := shouldUseBlobCache(); useBlob {
@@ -144,32 +139,119 @@ func shouldUseBlobCache() (string, bool) {
 	return "", false
 }
 
-// Roundtrip handles caching and ratelimiting of responses from GitHub.
-func (gh *RateLimitRoundTripper) RoundTrip(r *http.Request) (*http.Response, error) {
-	resp, err := gh.InnerTransport.RoundTrip(r)
-	if err != nil {
-		return nil, errors.Wrap(err, "error in round trip")
-	}
+// RateLimitTransport implements GitHub's best practices
+// for avoiding rate limits
+// https://developer.github.com/v3/guides/best-practices-for-integrators/#dealing-with-abuse-rate-limits
+type RateLimitTransport struct {
+	Logger           *zap.SugaredLogger
+	transport        http.RoundTripper
+	delayNextRequest bool
+}
 
-	rateLimit := resp.Header.Get("X-RateLimit-Remaining")
-	remaining, err := strconv.Atoi(rateLimit)
-	if err != nil {
-		return resp, nil
-	}
+func (rlt *RateLimitTransport) RoundTrip(req *http.Request) (*http.Response, error) {
+	// Make requests for a single user or client ID serially
+	// This is also necessary for safely saving
+	// and restoring bodies between retries below
+	rlt.lock(req)
 
-	if remaining <= 0 {
-		reset, err := strconv.Atoi(resp.Header.Get("X-RateLimit-Reset"))
-		if err != nil {
-			return resp, nil
+	// If you're making a large number of POST, PATCH, PUT, or DELETE requests
+	// for a single user or client ID, wait at least one second between each request.
+	/*
+		if rlt.delayNextRequest {
+			if rlt.Logger != nil {
+				rlt.Logger.Warnf("[DEBUG] Sleeping %s between write operations", writeDelay)
+			}
+			time.Sleep(writeDelay)
 		}
 
-		duration := time.Until(time.Unix(int64(reset), 0))
-		gh.Logger.Warnf("Rate limit exceeded. Waiting %s to retry...", duration)
+		rlt.delayNextRequest = isWriteMethod(req.Method)
+	*/
 
-		// Retry
-		time.Sleep(duration)
-		gh.Logger.Warnf("Rate limit exceeded. Retrying...")
-		return gh.RoundTrip(r)
+	resp, err := rlt.transport.RoundTrip(req)
+	if err != nil {
+		rlt.unlock(req)
+		return resp, errors.Wrap(err, "error in http roundtrip for response")
 	}
+
+	// Make response body accessible for retries & debugging
+	// (work around bug in GitHub SDK)
+	// See https://github.com/google/go-github/pull/986
+	r1, r2, err := drainBody(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	resp.Body = r1
+	ghErr := github.CheckResponse(resp)
+	resp.Body = r2
+
+	// When you have been limited, use the Retry-After response header to slow down.
+	if arlErr, ok := ghErr.(*github.AbuseRateLimitError); ok {
+		rlt.delayNextRequest = false
+		retryAfter := arlErr.GetRetryAfter()
+		if rlt.Logger != nil {
+			rlt.Logger.Warnf("Abuse detection mechanism triggered, sleeping for %s before retrying", retryAfter)
+		}
+		time.Sleep(retryAfter)
+		rlt.unlock(req)
+		return rlt.RoundTrip(req)
+	}
+
+	if rlErr, ok := ghErr.(*github.RateLimitError); ok {
+		rlt.delayNextRequest = false
+		retryAfter := time.Until(rlErr.Rate.Reset.Time)
+		if rlt.Logger != nil {
+			rlt.Logger.Warnf("[DEBUG] Rate limit %d reached, sleeping for %s (until %s) before retrying",
+				rlErr.Rate.Limit, retryAfter, time.Now().Add(retryAfter))
+		}
+		time.Sleep(retryAfter)
+		rlt.unlock(req)
+		return rlt.RoundTrip(req)
+	}
+
+	rlt.unlock(req)
+
 	return resp, nil
+}
+
+func (rlt *RateLimitTransport) lock(req *http.Request) {
+	ctx := req.Context()
+	if rlt.Logger != nil {
+		rlt.Logger.Debugf("[TRACE] Acquiring lock for GitHub API request (%q)", ctx)
+	}
+}
+
+func (rlt *RateLimitTransport) unlock(req *http.Request) {
+	ctx := req.Context()
+	if rlt.Logger != nil {
+		rlt.Logger.Debugf("[TRACE] Releasing lock for GitHub API request (%q)", ctx)
+	}
+}
+
+func NewRateLimitTransport(rt http.RoundTripper) *RateLimitTransport {
+	return &RateLimitTransport{transport: rt}
+}
+
+// drainBody reads all of b to memory and then returns two equivalent
+// ReadClosers yielding the same bytes.
+func drainBody(b io.ReadCloser) (r1, r2 io.ReadCloser, err error) {
+	if b == http.NoBody {
+		// No copying needed. Preserve the magic sentinel meaning of NoBody.
+		return http.NoBody, http.NoBody, nil
+	}
+	var buf bytes.Buffer
+	if _, err = buf.ReadFrom(b); err != nil {
+		return nil, b, errors.Wrap(err, "unable to read from buffer")
+	}
+	if err := b.Close(); err != nil {
+		return nil, b, errors.Wrap(err, "error while closing the buffer")
+	}
+	return ioutil.NopCloser(&buf), ioutil.NopCloser(bytes.NewReader(buf.Bytes())), nil
+}
+
+func isWriteMethod(method string) bool {
+	switch method {
+	case "POST", "PATCH", "PUT", "DELETE":
+		return true
+	}
+	return false
 }
